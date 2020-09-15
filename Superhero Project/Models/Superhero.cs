1a1ee5e2bf58a8fad4aa02d20facbb9e1e3b5a51
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-15 10:48:38 -0500

    Innit.

---
+++ Superhero Project/Models/Superhero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superhero_Project.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
